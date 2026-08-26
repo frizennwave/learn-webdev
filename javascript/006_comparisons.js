522f7ee913d0872bb8d247ec84fa2bc00b6a753a
frizennwave <hani3605fudin@gmail.com>
2026-08-26 10:00:49 +0700

    day-two

---
+++ javascript/006_comparisons.js
@@ -0,0 +1,11 @@
+
+// 1. Boolean is the result
+// 2. String comparison
+// 3. Comparison of different types
+// 4. Strict equality
+// 5. Comparison with null and undefined
+
+
+
+
+
---
+++ javascript/007_conditional-branching.js
@@ -0,0 +1,14 @@
+
+// 1. If statement
+// 2. Boolean conversion
+// 3. The else clause
+// 4. Several conditions: "else if"
+// 5. Conditional operator "?"
+// 6. Multiple "?"
+// 7. Non-traditional use of "?"
+
+
+
+
+
+
